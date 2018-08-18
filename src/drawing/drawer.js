74e0843f91a2a271e41f538c210e7ca00329a471
Mark Vickers <msvick@sandia.gov>
2018-08-18 09:49:05 -0600

    Using javascript classes

--- src/drawing/drawer.js
+++ src/drawing/drawer.js
@@ -2,42 +2,45 @@
 
 
 // Pattern:  constructor function
-function Drawer(viewer) {
-    this.viewer = viewer;
-    this.isDragging = false;
-    // Ellipsoid:  A quadratic surface defined in Cartesian coordinates by the equation (x / a)^2 + (y / b)^2 + (z / c)^2 = 1.
-    // Primarily used by Cesium to represent the shape of planetary bodies.
-    this.ellipsoid = viewer.scene.globe.ellipsoid;
-    this.startDragPosition;
-    this.startPos = {};
+class Drawer {
 
-    // Add a rectangle entity
+    constructor(viewer){
+        this.viewer = viewer;
+        this.isDragging = false;
+        // Ellipsoid:  A quadratic surface defined in Cartesian coordinates by the equation (x / a)^2 + (y / b)^2 + (z / c)^2 = 1.
+        // Primarily used by Cesium to represent the shape of planetary bodies.
+        this.ellipsoid = viewer.scene.globe.ellipsoid;
+        this.startDragPosition;
+        this.startPos = {};
     
-    this.rectangleEntity = viewer.entities.add({
-        id: 'selection_rectangle',
-        rectangle: {
-            fill: false,
-            outline: true,
-            outlineColor: Cesium.Color.RED, //  .fromCssColorString(getSelectedColor()),
-            show: false,
-        },
-    });
-
+        // Add a rectangle entity
+        
+        this.rectangleEntity = viewer.entities.add({
+            id: 'selection_rectangle',
+            rectangle: {
+                fill: false,
+                outline: true,
+                outlineColor: Cesium.Color.RED, //  .fromCssColorString(getSelectedColor()),
+                show: false,
+            },
+        });    
+    }
+    
     
 
-    this.setupHandlers = () => {
+    setupHandlers() {
 
         Cesium.ScreenSpaceEventHandler.mouseEmulationIgnoreMilliseconds = 1600; // default = 800
 
-        var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+        var handler = new Cesium.ScreenSpaceEventHandler(this.viewer.canvas);
         
-        handler.setInputAction(this.ctrlLeftDown, Cesium.ScreenSpaceEventType.LEFT_DOWN, Cesium.KeyboardEventModifier.CTRL);
-        handler.setInputAction(this.ctrlLeftUp, Cesium.ScreenSpaceEventType.LEFT_UP, Cesium.KeyboardEventModifier.CTRL);
-        handler.setInputAction(this.ctrlMove, Cesium.ScreenSpaceEventType.MOUSE_MOVE, Cesium.KeyboardEventModifier.CTRL);
+        handler.setInputAction(this.ctrlLeftDown.bind(this), Cesium.ScreenSpaceEventType.LEFT_DOWN, Cesium.KeyboardEventModifier.CTRL);
+        handler.setInputAction(this.ctrlLeftUp.bind(this), Cesium.ScreenSpaceEventType.LEFT_UP, Cesium.KeyboardEventModifier.CTRL);
+        handler.setInputAction(this.ctrlMove.bind(this), Cesium.ScreenSpaceEventType.MOUSE_MOVE, Cesium.KeyboardEventModifier.CTRL);
 
-    };
+    }
 
-    this.setRectangleCoord = (nesw) => {
+    setRectangleCoord(nesw){
         this.rectangleEntity.rectangle.coordinates = Cesium.Rectangle.fromDegrees(
             nesw.west,
             nesw.south,
@@ -47,15 +50,15 @@ function Drawer(viewer) {
     };
 
 
-    this.suspendPanZoom = () => {
+    suspendPanZoom() {
         this.viewer.scene.screenSpaceCameraController.enableInputs = false;
     };
 
-    this.resumePanZoom = () => {
+    resumePanZoom() {
         this.viewer.scene.screenSpaceCameraController.enableInputs = true;
     };
   
-    this.getLatLon = (cartesian) => {
+    getLatLon(cartesian) {
         const cartographic = this.ellipsoid.cartesianToCartographic(cartesian); // cartographic is in radians
         const lon = parseFloat(Cesium.Math.toDegrees(cartographic.longitude).toFixed(5));
         const lat = parseFloat(Cesium.Math.toDegrees(cartographic.latitude).toFixed(5));
@@ -65,7 +68,7 @@ function Drawer(viewer) {
         };
     }
 
-    this.startDrag = (position) => {
+    startDrag(position) {
 
         this.suspendPanZoom();
 
@@ -91,17 +94,17 @@ function Drawer(viewer) {
         this.rectangleEntity.rectangle.show = true;
     };
 
-    this.ctrlLeftDown = (click) => {
+    ctrlLeftDown(click) {
         this.startDragPosition = click.position;
 
         this.startDrag(this.startDragPosition);
     };
 
-    this.ctrlLeftUp = (click) => {
+    ctrlLeftUp(click) {
         this.endDrag(click.position);
     };
 
-    this.endDrag = (position) => {
+    endDrag(position) {
         this.resumePanZoom();
 
         this.isDragging = false;
@@ -119,7 +122,7 @@ function Drawer(viewer) {
 
     };
 
-    this.move = (movement) => {
+    move(movement) {
         const position = movement.endPosition;
 
         if (this.isDragging) {
@@ -127,7 +130,7 @@ function Drawer(viewer) {
         }
     };
 
-    this.updateDrag = (position) => {
+    updateDrag(position) {
         const cartesian = this.getCartesian(position);
         if (!cartesian || !this.startDragPosition) {
             return;
@@ -139,15 +142,15 @@ function Drawer(viewer) {
         this.setRectangleCoord(nesw);
     };
 
-    this.ctrlMove = (movement) => {
+    ctrlMove(movement) {
         this.move(movement);
     };
 
-    this.getCartesian = (position) => {
-        return viewer.camera.pickEllipsoid(position, this.ellipsoid);
+    getCartesian(position) {
+        return this.viewer.camera.pickEllipsoid(position, this.ellipsoid);
     }
 
-    this.getBoundingBox = (position1, position2) => {
+    getBoundingBox(position1, position2) {
 
         const east = Math.max(position1.longitude, position2.longitude);
         const west = Math.min(position1.longitude, position2.longitude);
